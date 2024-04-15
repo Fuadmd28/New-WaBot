75f9a52edbcc8eb712c3f5aa26e5050638e63044
Papah-Chan <92633211+FahriAdison@users.noreply.github.com>
2024-04-15 09:24:04 +0700

    Pakai scrape jika api eror

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,3 +1,4 @@
+import tio from 'btch-downloader'
 import fetch from 'node-fetch'; 
 import fg from 'api-dylux';
 
@@ -6,6 +7,18 @@ let handler = async (m, { conn, text, args, usedPrefix, command }) => {
     if (!args[0].match(/tiktok/gi)) throw `Make sure the link is from TikTok`;
     conn.reply(m.chat, global.wait, m);
     try {
+            let json = await tio.ttdl((args[0])) 
+            let fetchStartTime = new Date();
+            let fetchResponse = await fetch(args[0]);
+            let fetchEndTime = new Date();
+            let fetchTime = fetchEndTime - fetchStartTime;
+            let caption = `乂  *T I K T O K*\n\n`;
+        caption += `${json.title || '-'}\n`;
+        caption += `	◦  *Fetching Time* : ${fetchTime} ms`;        
+            conn.sendFile(m.chat, json.video, '', caption, m);                       
+    } catch {
+    m.reply('*☓ An unexpected error occurred*');
+        try {
             let res = await fetch(global.API('marin', '/api/download/tiktok', {
                 url: args[0]
             }, 'apikey'));
@@ -22,21 +35,6 @@ let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 
             caption += `	◦  *Caption* : ${json.result.description || '-'}`;
             conn.sendFile(m.chat, json.result.play, '', caption, m);
-    } catch {
-        try {
- let fetchStartTime = new Date();
-        let fetchResponse = await fetch(args[0]);
-        let fetchEndTime = new Date();
-        let fetchTime = fetchEndTime - fetchStartTime;
-        let p = await fg.tiktok(args[0]);
-        let caption = `乂  *T I K T O K*\n\n`;
-        caption += `	◦  *Author* : ${p.nickname} (@${p.unique_id})\n`;
-        caption += `	◦  *Download Count* : ${p.download_count}\n`;
-        caption += `	◦  *Duration* : ${p.duration}\n`;
-        caption += `	◦  *Fetching Time* : ${fetchTime} ms\n`;
-
-        caption += `	◦  *Caption* : ${p.description || '-'}`;
-        conn.sendFile(m.chat, p.hdplay, 'tiktok.mp4', caption, m);
     } catch {
             m.reply('*☓ An unexpected error occurred*');
         }
